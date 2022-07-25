ffd1a4a328d8e44226883146db19e9c36e73350d
Marcel <Marcel@Marcels-MacBook-Pro.local>
2022-07-25 09:51:20 -0400

    intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,6 +2,22 @@ import "./intro.scss"
 
 export default function Intro() {
   return (
-    <div className="intro" id="intro"></div>
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="man"></img>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi, I'm</h2>
+          <h1>Marcel</h1>
+          <h3><span>Freelancer</span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down"></img>
+        </a>
+      </div>
+    </div>
   )
 }
